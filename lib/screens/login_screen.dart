193e5baf14923cbafff2f4fed35485a86d9d734b
Stanislav Sitdikov <sidies@mail.ru>
2021-05-31 13:48:58 +0700

    finished log in, log out with Firebase

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'chat_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
@@ -65,8 +66,16 @@ class _LoginScreenState extends State<LoginScreen> {
                 color: Colors.lightBlueAccent,
                 onPressed: () async {
                   //Implement login functionality.
-                  _auth.signInWithEmailAndPassword(
-                      email: email, password: password);
+
+                  try {
+                    final user = await _auth.signInWithEmailAndPassword(
+                        email: email, password: password);
+                    if (user != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                  } catch (e) {
+                    print(e);
+                  }
                 },
                 title: 'Log In'),
           ],
